4a52e0a55538439fa7df0ca4d3c41bd0434be1d3
marcelkuyyy <103729319+marcelkuyyy@users.noreply.github.com>
2023-03-03 21:43:32 +0700

    menambahkan file pertemuan 3
    menambahkan file pertemuan 3

---
+++ pkg/mod.py
@@ -0,0 +1,11 @@
+
+
+s = "Tutorial python kampus merdeka"
+mylist = [10,20,30,40]
+
+def fungsi_kostum(x, y,z):
+    return x**2 + y*z
+
+if __name__ == '__main__':
+    print(fungsi_kostum(2,3,4))
+    print("Ini menjalankan mod.py")
